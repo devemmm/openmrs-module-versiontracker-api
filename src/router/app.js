41096b7307a7b01e0ba52973bfba92a80840e441
devemmm <djntivuguruzwaemmanuel@gmail.com>
2023-06-15 10:10:34 +0200

    renaming routes for common

--- src/router/app.js
+++ src/router/app.js
@@ -2,17 +2,17 @@ const express = require('express')
 const router = express.Router()
 const Controller = require('../controller/app')
 
-router.post('/user/signin', new Controller().signin)
+router.post('/module/api/signin', new Controller().signin)
 
-router.post('/user/signup', new Controller().signup)
+router.post('/module/api/signup', new Controller().signup)
 
-router.get('/facilities', new Controller().facilityInfo)
+router.get('/module/api/facilities', new Controller().facilityInfo)
 
-router.get('/facilities/:id', new Controller().facilityInfo)
+router.get('/module/api/facilities/:id', new Controller().facilityInfo)
 
-router.post('/user/register/facility', new Controller().registerFacility)
+router.post('/module/api/register/facility', new Controller().registerFacility)
 
-router.post('/user/addupdatemodule', new Controller().addUpdateModule)
+router.post('/module/api/user/addupdatemodule', new Controller().addUpdateModule)
 
 
 
